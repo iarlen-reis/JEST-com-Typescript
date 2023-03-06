145c37dce299e776b1291df19d84af78d6da566b
= <iarlensr@hotmail.com>
2023-03-06 16:28:29 -0300

    introdução a testes unitário utilizando jest.

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe("TESTANDO ALGUMA COISA", () => {
+  it("should return one", () => {
+    const number = 1;
+
+    expect(number).toBe(1);
+  });
+
+  test("should return Iarlen", () => {
+    const name = "Iarlen";
+
+    expect(name).toBe("Iarlen");
+  });
+});
